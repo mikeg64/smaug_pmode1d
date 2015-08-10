91be796ea3c03bcc229d8ef70f47be23508ea7df
mikeg64 <m.griffiths@sheffield.ac.uk>
2015-08-10 21:18:23 +0100

    First updates for working 1d pmode model with VALIIIc solar atmosphere

---
+++ runphotwave1d_180.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#$ -j y
+#$ -l arch=intel*
+#$ -l gpu=1
+##$ -l gpu_arch=nvidia-m2070
+#$ -l gpu=1,gpu_arch=nvidia-k40m
+#$ -P gpu
+#$ -N p180_1d
+#$ -l mem=12G
+#$ -l rmem=12G
+#$ -l h_rt=168:00:00
+module load libs/cuda/6.5.14
+
+
+
+cd include
+cp iosmaugparams_180_1d.h iosmaugparams.h
+cp initialisation_user_p_1d.h initialisation_user.h
+
+cd ..
+
+cd src
+cp usersource_180_1d.cu usersource.cu
+cp boundary_p_1d.cu boundary.cu
+
+
+make clean
+make -f Makefile smaug
+cd ..
+
+
+
+export TIMECOUNTER=0
+source timeused
+./smaug a
+source timeused
+
